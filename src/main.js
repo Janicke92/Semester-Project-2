7a38be6f3ef90e93e6e0b51a7ebd6734fcc83a36
Janicke92 <142732047+Janicke92@users.noreply.github.com>
2026-05-05 13:54:06 +0200

    Update to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { initRegisterPage } from './pages/register.js';
 import { initLoginPage } from './pages/login.js';
+import { initProfilePage } from './pages/profile.js';
+import { initHomePage } from './pages/index.js';
 
 initRegisterPage();
 
 initLoginPage();
+
+initProfilePage();
+
+initHomePage();
