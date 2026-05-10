1cd8d6d0eef0661ef25e9712c0e1792ef454136a
Janicke92 <142732047+Janicke92@users.noreply.github.com>
2026-05-10 19:16:41 +0200

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,5 @@ export const API_BASE_URL = 'https://v2.api.noroff.dev';
 export const AUTH_URL = `${API_BASE_URL}/auth`;
 export const REGISTER_URL = `${AUTH_URL}/register`;
 export const LOGIN_URL = `${AUTH_URL}/login`;
-export const API_KEY_URL = `${AUTH_URL}/create-api-key`;
 
-// TODO: Remove API Key before delivery
-export const TEMP_API_KEY = 'd4744329-2f35-4c55-9a3c-274e7f527f2b';
+export const API_KEY = 'd4744329-2f35-4c55-9a3c-274e7f527f2b';
